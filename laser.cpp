28e4a1b260218d3b4af0bde70391709fba192eed
Alana-Silva <alanaingridf@icloud.com>
2023-02-04 20:18:00 -0300

    Definição da classe Laser

---
+++ laser.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include "ambiente.hpp"
+using namespace std;
+
+class Laser {
+    public: 
+        void calcularColisoes(Ambiente amb);
+};
+
+void Laser::calcularColisoes(Ambiente amb) {
+//O laser detecta a presen ca de um obstaculo para qualquer
+//célula adjacente
+};
---
+++ laser.hpp
@@ -0,0 +1,15 @@
+#ifndef LASER_H
+#define LASER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "ambiente.hpp"
+
+class Laser {
+public:
+void calcularColisoes(Ambiente amb);
+};
+
+#endif //LASER_H
+
