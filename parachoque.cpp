444f05dbfddaaa828f53247fcf9e3f624fa8e11a
Alana-Silva <alanaingridf@icloud.com>
2023-02-04 20:18:29 -0300

    Definição da classe parachoque

---
+++ parachoque.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include "ambiente.hpp"
+using namespace std;
+
+class Parachoque {
+    public:
+        void calcularColisoes(Ambiente amb);
+};
+
+void Parachoque::calcularColisoes(Ambiente amb) {
+    //detecta presenca de obstaculos na direcao do movimento
+
+}
---
+++ parachoque.hpp
@@ -0,0 +1,15 @@
+#ifndef PARACHOQUE_HPP
+#define PARACHOQUE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include "ambiente.hpp"
+using namespace std;
+
+class Parachoque {
+    public:
+        void calcularColisoes(Ambiente amb);
+};
+
+#endif 
